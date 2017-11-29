6c4229825c450600c2d81115a954bf3e50277051
FeL1ksS <fel1kssx@gmail.com>
2017-11-29 21:35:56 +0300

    list rest

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     var swiper = new Swiper('.swiper-container', {
         slidesPerView: 1,
         autoHeight: true, //enable auto height
-        // spaceBetween: 1000,
+        spaceBetween: 0,
         loop: true,
         autoplay: {
             delay: 3000,
