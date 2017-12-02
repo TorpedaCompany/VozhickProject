431b72fab009fb195deb3c5771e16add415a3577
Icon003 <Icon003@yandex.by>
2017-12-02 18:49:53 +0300

    Header v1.0

---
+++ js/hamburger-menu.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+    var HamburgerMenuOpen = document.getElementById("hamburger-menu-open");
+    var HamburgerMenuClose = document.getElementById("hamburger-menu-close");
+    var HamburgerMenu = document.getElementById("hamburger-menu");
+    
+    HamburgerMenuOpen.addEventListener("click", function(){
+        HamburgerMenu.classList.remove("close");
+        HamburgerMenu.classList.add("open");
+//        HamburgerMenu.style.cssText = "visibility: visible; transition: top 1s ease-out 3s;";
+        console.log("Меню открыто");
+        
+    });
+    
+    HamburgerMenuClose.addEventListener("click", function(){
+        HamburgerMenu.classList.remove("open");
+        HamburgerMenu.classList.add("close");
+//        HamburgerMenu.style.cssText = "visibility: hidden; transition: top 1s ease-out 3s;";
+        console.log("Меню закрыто");
+    });
+});
