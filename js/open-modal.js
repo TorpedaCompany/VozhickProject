9a8f6cc0bff917a348222656c66d16f78c26766e
Icon003 <Icon003@yandex.by>
2017-12-03 13:39:03 +0300

    Modal v1.0

--- js/open-modal.js
+++ js/open-modal.js
@@ -9,34 +9,39 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var ModalRegistration = document.getElementById("modal-registration");
     
     ModalAuthorizationOpenTwo.addEventListener("click", function(){
+        $("html,body").css("overflow","hidden");
         ModalRegistration.classList.remove("modal-active");
         Ovelay.classList.add("ovelay-active");
         ModalAuthorization.classList.add("modal-active");
     });
     
     ModalAuthorizationOpen.addEventListener("click", function(){
-//        ModalRegistration.classList.remove("modal-active");
+        $("html,body").css("overflow","hidden");
         Ovelay.classList.add("ovelay-active");
         ModalAuthorization.classList.add("modal-active");
     });
     
     ModalAuthorizationClose.addEventListener("click", function(){
+        $("html,body").css("overflow","auto");
         ModalAuthorization.classList.remove("modal-active");
         Ovelay.classList.remove("ovelay-active");
     });
     
     ModalAuthorizationClose.addEventListener("click", function(){
+        $("html,body").css("overflow","auto");
         ModalAuthorization.classList.remove("modal-active");
         Ovelay.classList.remove("ovelay-active");
     });
     
     
     ModalRegistrationOpen.addEventListener("click", function(){
+        $("html,body").css("overflow","hidden");
         ModalAuthorization.classList.remove("modal-active");
         ModalRegistration.classList.add("modal-active");
     });
     
     ModalRegistrationClose.addEventListener("click", function(){
+        $("html,body").css("overflow","auto");
         ModalRegistration.classList.remove("modal-active");
         Ovelay.classList.remove("ovelay-active");
     });
