188401e122ae7b26892732811e80f24498202eb1
Icon003 <Icon003@yandex.by>
2017-12-03 13:14:44 +0300

    a

---
+++ js/open-modal.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    var ModalAuthorizationOpen = document.getElementById("hamburger-menu-open");
+    var ModalAuthorizationClose = document.getElementById("hamburger-menu-close");
+    var ModalRegistrationOpen = document.getElementById("hamburger-menu-open");
+    var ModalRegistrationClose = document.getElementById("hamburger-menu-close");
+    var Ovelay = document.getElementById("ovelay");
+});
