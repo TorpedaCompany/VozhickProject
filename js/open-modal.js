4e9cc3e4a88e1630b43d16dc56aef895a83e2d7e
Alexander <fel1kssx@gmail.com>
2017-12-08 02:56:09 +0300

    modal window dish v0.5

--- js/open-modal.js
+++ js/open-modal.js
@@ -9,6 +9,30 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var ModalRegistration = document.getElementById("modal-registration");
     var keys = {37: 1, 38: 1, 39: 1, 40: 1};
 
+    
+
+    function addEventModalDish(){
+        let modalDish = document.querySelectorAll(".rest-card-dish");
+    
+        for(let i = 0; i < modalDish.length; i++){
+            modalDish[i].addEventListener("click", displayModalDish, false);
+        }
+    
+        function displayModalDish(e){
+            e = e || event;
+            target = e.target.parentNode.className || e.srcElement.parentNode.className;
+            if(target != "card-dish-action" && target != "addToCard"){
+                disableScroll();
+                let cloneDishes = this.cloneNode(true);
+                cloneDishes.classList.add('modalDish');
+                Ovelay.classList.add("ovelay-active");
+                Ovelay.appendChild(cloneDishes);
+            }
+        }
+    }
+    addEventModalDish();
+
+
     function preventDefault(e) {
         e = e || window.event;
         if (e.preventDefault)
@@ -85,6 +109,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
             ModalAuthorization.classList.remove("modal-active");
             ModalRegistration.classList.remove("modal-active");
             Ovelay.classList.remove("ovelay-active");
+
+            if(Ovelay.lastChild.className == "rest-card-dish modalDish"){
+                Ovelay.removeChild(Ovelay.lastChild);
+            }
         }
     });
 });
--- js/rest.js
+++ js/rest.js
@@ -1,18 +1,3 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-
-    // let rest_filter = document.querySelector(".rest-filter-container");
-    // if (document.documentElement.scrollTop != 0) {
-    //     rest_filter.classList.add("sticky");
-    // }
-
-    // window.addEventListener('scroll', function() {
-    //     if (document.documentElement.scrollTop > 100) {
-    //         rest_filter.classList.add("sticky");
-    //     } else {
-    //         rest_filter.classList.remove("sticky");
-    //     }
-    //     // console.log(document.documentElement.scrollTop);
-    // });
-    
 });
 var bLazy = new Blazy({});
