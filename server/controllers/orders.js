f1ce2b16ec198a0ea45873a55a468e6134412f64
Alexander <fel1kssx@gmail.com>
2017-12-30 03:02:45 +0300

    controller orders 0.4

--- server/controllers/orders.js
+++ server/controllers/orders.js
@@ -3,25 +3,64 @@ let app = new(require('express').Router)();
 const models = require('../database/models');
 
 app.get('/orders', (req, res) => {
-    models.orders.find({}, function(err, users) {
-        res.send(users.reduce(function(userMap, item) {
-            userMap[item.id] = item;
-            return userMap;
-        }, {}));
+    models.orders.find({}, function(err, data) {
+        if (err)
+            return res.status(500).send({ error: err });
+        else
+            return res.status(200).send(data);
     });
 })
-app.get('/orders/:name', (req, res) => {
-    let user = req.params.name;
-    // console.log(user);
-    models.orders.find({ 'login': user }, function(err, data) {
-        res.send(data);
+app.get('/orders/:id', (req, res) => {
+    models.orders.findById(req.params.id, function(err, data) {
+        if (err)
+            return res.status(500).send({ error: err.message });
+        if (!data)
+            return res.status(404).send({ error: "Not found" });
+        else
+            return res.status(200).send(data);
     });
 })
-
-
-// db.collection('pending_orders').find({}).toArray(function(error, documents) {
-//     if (err) throw error;
-//     res.send(documents);
-//     console.log("Отправлены данные --" + new Date());
-// });
+app.post('/orders', (req, res) => {
+    let order = new models.orders();
+    for (key in req.body) {
+        order[key] = req.body[key];
+    }
+    order.save(function(err, data) {
+        if (err)
+            return res.status(500).send({ error: err.message });
+        else
+            return res.status(200).send(data._id);
+    })
+})
+app.put('/orders/:id', (req, res) => {
+    models.orders.findById(req.params.id, function(err, data) {
+        if (err)
+            return res.status(500).send({ error: err.message });
+        if (!data)
+            return res.status(404).send({ error: "Not found" });
+        for (key in req.body) {
+            data[key] = req.body[key];
+        }
+        data.save(function(err, data) {
+            if (err)
+                return res.status(500).send({ error: err.message });
+            else
+                return res.status(200).send(data._id);
+        });
+    })
+})
+app.delete('/orders/:id', (req, res) => {
+    models.orders.findById(req.params.id, function(err, data) {
+        if (err)
+            return res.status(500).send({ error: err.message });
+        if (!data)
+            return res.status(404).send({ error: "Not found" });
+        data.remove(function(err, data) {
+            if (err)
+                return res.status(500).send({ error: err.message });
+            else
+                return res.status(200).send(data._id);
+        });
+    });
+})
 module.exports = app;
