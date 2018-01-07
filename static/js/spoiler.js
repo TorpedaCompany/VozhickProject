e6eaa62cefb1986984194aadd81c206e3b272f1e
Icon003 <Icon003@yandex.by>
2018-01-07 03:59:21 +0300

    Bly

---
+++ static/js/spoiler.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+    var Spoiler = document.getElementsByClassName("spoiler");
+    console.log(Spoiler);
+    
+    function spoilerfunc(){
+        let Child = this.querySelector('.content-spoiler');
+        Child.classList.toggle('open-spoiler');
+    }
+    for(let i = 0; i < Spoiler.length; i++){
+        Spoiler[i].addEventListener("click", spoilerfunc, false);
+    }
+        
+    
+});
