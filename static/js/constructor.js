d9bd1680fad8918863ddbad00ed802ebc918cd4b
Icon003 <Icon003@yandex.by>
2018-01-16 00:13:26 +0300

    Constructor

--- static/js/constructor.js
+++ static/js/constructor.js
@@ -5,17 +5,14 @@ function Update() {
     var MinusIngredients = document.getElementsByClassName("ingredients-items-minus");
     var ContainerCunstructorMy = document.getElementById("my-ingredients");
     var ContainerCunstructor = document.getElementById("ingredients");
-
+	var ButtonClearCunstructor = document.getElementById("button-remove-pancake");
 
     function PlusIngridient() {
         let Elem = this;
-        let ElemSpan = Elem.querySelector('.plus-ingredients');
-        Elem.classList.remove("ingredients-items-plus");
-        ElemSpan.classList.remove("fa-plus-square");
-        Elem.classList.add("ingredients-items-minus");
-        ElemSpan.classList.add("fa-minus-square");
-        ContainerCunstructorMy.appendChild(Elem);
-        Update()
+        let ElemId = Elem.getAttribute('data-id-ingredients');
+		let MyIngredients = ContainerCunstructorMy.querySelector('[data-id-ingredients='+ElemId+']');
+		Elem.classList.add('ingredients-items-hidden');
+		MyIngredients.classList.add('ingredients-items-active');
     }
     for (let i = 0; i < PlusIngredients.length; i++) {
         PlusIngredients[i].addEventListener("click", PlusIngridient, false);
@@ -23,15 +20,28 @@ function Update() {
 
     function MinusIngridient() {
         let Elem = this;
-        let ElemSpan = Elem.querySelector('.plus-ingredients');
-        Elem.classList.remove("ingredients-items-minus");
-        ElemSpan.classList.remove("fa-minus-square");
-        Elem.classList.add("ingredients-items-plus");
-        ElemSpan.classList.add("fa-plus-square");
-        ContainerCunstructor.appendChild(Elem);
-        Update()
+        let ElemId = Elem.getAttribute('data-id-ingredients');
+		let Ingredients = ContainerCunstructor.querySelector('[data-id-ingredients='+ElemId+']');
+		Elem.classList.remove('ingredients-items-active');
+		Ingredients.classList.remove('ingredients-items-hidden');
+		
     }
     for (let i = 0; i < MinusIngredients.length; i++) {
         MinusIngredients[i].addEventListener("click", MinusIngridient, false);
     }
+	
+	ButtonClearCunstructor.addEventListener("click", function () {
+//		console.log(MinusIngredients);
+		let ActiveIngredients = ContainerCunstructor.querySelectorAll('.ingredients-items-hidden');
+		let ActiveIngredientsMy = ContainerCunstructorMy.querySelectorAll('.ingredients-items-active');
+		let Input = document.getElementsByClassName('dishCount');
+		console.log(Input);
+		for(let i =0; i<ActiveIngredientsMy.length; i++){
+			ActiveIngredientsMy[i].classList.remove('ingredients-items-active');
+			ActiveIngredients[i].classList.remove('ingredients-items-hidden');
+			
+		}
+		Input.value = 1;
+//		ActiveIngredients.classList.remove('ingredients-items-active');
+	});
 }
