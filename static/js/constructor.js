5853fc175c67ad726485624dc8f3324a28a19e43
Icon003 <Icon003@yandex.by>
2018-01-16 18:12:10 +0300

    Con

--- static/js/constructor.js
+++ static/js/constructor.js
@@ -1,43 +1,91 @@
 document.addEventListener("DOMContentLoaded", Update());
 
 function Update() {
-    var PlusIngredients = document.getElementsByClassName("ingredients-items-plus");
-    var MinusIngredients = document.getElementsByClassName("ingredients-items-minus");
-    var ContainerCunstructorMy = document.getElementById("my-ingredients");
-    var ContainerCunstructor = document.getElementById("ingredients");
+	var PlusIngredients = document.getElementsByClassName("ingredients-items-plus");
+	var MinusIngredients = document.getElementsByClassName("ingredients-items-minus");
+	var ContainerCunstructorMy = document.getElementById("my-ingredients");
+	var ContainerCunstructor = document.getElementById("ingredients");
 	var ButtonClearCunstructor = document.getElementById("button-remove-pancake");
-
-    function PlusIngridient() {
-        let Elem = this;
-        let ElemId = Elem.getAttribute('data-id-ingredients');
-		let MyIngredients = ContainerCunstructorMy.querySelector('[data-id-ingredients='+ElemId+']');
+	var ButtonPostCunstructor = document.getElementById("button-cart-post");
+	function PlusIngridient() {
+		let Elem = this;
+		let ElemId = Elem.getAttribute('data-id-ingredients');
+		let MyIngredients = ContainerCunstructorMy.querySelector('[data-id-ingredients=' + ElemId + ']');
 		Elem.classList.add('ingredients-items-hidden');
 		MyIngredients.classList.add('ingredients-items-active');
-    }
-    for (let i = 0; i < PlusIngredients.length; i++) {
-        PlusIngredients[i].addEventListener("click", PlusIngridient, false);
-    }
+	}
+	for (let i = 0; i < PlusIngredients.length; i++) {
+		PlusIngredients[i].addEventListener("click", PlusIngridient, false);
+	}
 
-    function MinusIngridient() {
-        let Elem = this;
-        let ElemId = Elem.getAttribute('data-id-ingredients');
-		let Ingredients = ContainerCunstructor.querySelector('[data-id-ingredients='+ElemId+']');
+	function MinusIngridient() {
+		let Elem = this;
+		let ElemId = Elem.getAttribute('data-id-ingredients');
+		let Ingredients = ContainerCunstructor.querySelector('[data-id-ingredients=' + ElemId + ']');
 		Elem.classList.remove('ingredients-items-active');
 		Ingredients.classList.remove('ingredients-items-hidden');
-		
-    }
-    for (let i = 0; i < MinusIngredients.length; i++) {
-        MinusIngredients[i].addEventListener("click", MinusIngridient, false);
-    }
-	
-	ButtonClearCunstructor.addEventListener("click", function () {
+
+	}
+	for (let i = 0; i < MinusIngredients.length; i++) {
+		MinusIngredients[i].addEventListener("click", MinusIngridient, false);
+	}
+
+	ButtonClearCunstructor.addEventListener("click", function Clear() {
 		let ActiveIngredients = ContainerCunstructor.querySelectorAll('.ingredients-items-hidden');
 		let ActiveIngredientsMy = ContainerCunstructorMy.querySelectorAll('.ingredients-items-active');
 		let Input = document.querySelector('.dishCount');
-		for(let i =0; i<ActiveIngredientsMy.length; i++){
+		for (let i = 0; i < ActiveIngredientsMy.length; i++) {
 			ActiveIngredientsMy[i].classList.remove('ingredients-items-active');
 			ActiveIngredients[i].classList.remove('ingredients-items-hidden');
 		}
 		Input.value = 1;
+	});
+
+	ButtonPostCunstructor.addEventListener("click", function () {
+		let ActiveIngredientsMy = ContainerCunstructorMy.querySelectorAll('.ingredients-items-active');
+		let NameIngredients = '';
+		let PriceIngredients = 0;
+		let Cart = [];
+		let Arr = JSON.parse(localStorage.getItem('Cart'));
+		let Price = 0;
+		let Input = document.querySelector('.dishCount');
+		let CountDish = 0;
+		if (Arr == null) {
+			for (let i = 0; i < ActiveIngredientsMy.length; i++) {
+				NameIngredients += ActiveIngredientsMy[i].querySelector('.name-ingredients').innerText.concat(',');
+				Price = ActiveIngredientsMy[i].querySelector('.price-ingredients').innerText;
+				Price = Price.substr(0, Price.length - 5);
+				PriceIngredients += parseFloat(Price);
+
+			}
+			NameIngredients = NameIngredients.slice(0, -1);
+			NameIngredients = 'Блинчик ('+NameIngredients+')';
+			CountDish = Input.value;
+			Cart.push({
+				"Name": NameIngredients,
+				"Image": '../image/constructor/const_pancake.png',
+				"Price": PriceIngredients,
+				"CountDish": CountDish
+			});
+			var serialObj = JSON.stringify(Cart);
+			localStorage.setItem('Cart', serialObj);
+			console.log(NameIngredients);
+			console.log(PriceIngredients);
+			console.log(Cart);
+		} else {
+			let length = Arr.length;
+			for (let a = 0; a < length; a++) {
+				Cart.push(Arr[a]);
+			}
+			for (let i = 0; i < ActiveIngredientsMy.length; i++) {
+				NameIngredients += ActiveIngredientsMy[i].querySelector('.name-ingredients').innerText.concat(',');
+				Price = ActiveIngredientsMy[i].querySelector('.price-ingredients').innerText;
+				Price = Price.substr(0, Price.length - 5);
+				PriceIngredients += parseFloat(Price);
+
+			}
+			console.log(NameIngredients);
+			console.log(PriceIngredients);
+		}
 	});
 }
