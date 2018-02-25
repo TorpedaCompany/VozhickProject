3796cd546ddfd9476da2849d1289e5d99df5958a
Icon003 <Icon003@yandex.by>
2018-02-25 16:28:34 +0300

    fix cart

--- static/js/clear-cart.js
+++ static/js/clear-cart.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     var ClearButton = document.getElementById("button-cart-clear");
+    var AjaxCardButton = document.getElementById("button-cart-post");
     ClearButton.addEventListener("click", function () {
         var BlockContainer = document.getElementById("main-eat-container-cart");
         var BlockPriceContainer = document.getElementById("container-all-price-eat-cart");
-        var LS = JSON.parse(localStorage.getItem("Cart"));
         var div = "";
         var StrPrice = ""
         var Price = 0;
@@ -12,6 +12,10 @@ document.addEventListener("DOMContentLoaded", function () {
         StrPrice = 'Общая стоимость: 0 руб.';
         BlockContainer.innerHTML = div;
         BlockPriceContainer.innerHTML = StrPrice;
+		var LS = JSON.parse(localStorage.getItem("Cart"));
+		if(LS == null){
+			AjaxCardButton.classList.add("button-cart-post-disabled");
+		}
 		CountAndPriceCart();
     });
 });
