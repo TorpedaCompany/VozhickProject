298a304be21d623de69daf29d896b6a50935dd6f
Alexander <fel1kssx@gmail.com>
2018-02-28 20:09:06 +0300

    fix click rest ctg, var

--- static/js/index.js
+++ static/js/index.js
@@ -23,58 +23,59 @@ var bLazy = new Blazy({});
 
 //Обработчики на кнопки категорий выбора ресторана по их ID
 function addEventCtgRest() {
-    let arrCtgRest = document.querySelectorAll("#ctgRest > div");
+    var arrCtgRest = document.querySelectorAll("#ctgRest > div");
 
-    for (let i = 0; i < arrCtgRest.length; i++) {
+    for (var i = 0; i < arrCtgRest.length; i++) {
         arrCtgRest[i].addEventListener("click", selectCtg, false);
     }
+}
 
-    function rmActive() {
-        let activeArr = document.querySelectorAll(".type-restaurant_active");
-        for (i = 0; i < activeArr.length; i++) {
-            activeArr[i].classList.remove("type-restaurant_active");
-        }
-    }
-
-    function restCtg() {
-        alert('works!');
-        let id_rest = document.querySelectorAll("[data-id-rest]");
-        let ctg = [];
-        for (i = 0; i < id_rest.length; i++) {
-            ctg.push({
-                numRest: id_rest[i].dataset.idRest,
-                category: id_rest[i].dataset.ctgRest.toLowerCase().split(',')
-            })
-        }
-        return ctg;
-    }
-
-    function selectCtg() {
-        //Удалить все активные классы 
-        rmActive();
-        //Добавить активный класс
-        this.classList.toggle("type-restaurant_active");
-        //Показать все рестораны
-        fadeIn();
-
-        let ctg = restCtg();
-        let result = [];
-        let _id = this.id.toLowerCase();
-
-        //Проверка на категорию
-        if (this.id != "Все") {
-            ctg.forEach(function(item) {
-                if (!(item.category.indexOf(_id) != -1)) {
-                    // Скрыть ресторан если не подходит
-                    fadeOut(item.numRest);
-                }
-            })
-        }
+function rmActive() {
+    var activeArr = document.querySelectorAll(".type-restaurant_active");
+    for (i = 0; i < activeArr.length; i++) {
+        activeArr[i].classList.remove("type-restaurant_active");
     }
 }
+
+function restCtg() {
+    var id_rest = document.querySelectorAll("[data-id-rest]");
+    var ctg = [];
+    for (i = 0; i < id_rest.length; i++) {
+        ctg.push({
+            numRest: id_rest[i].dataset.idRest,
+            category: id_rest[i].dataset.ctgRest.toLowerCase().split(',')
+        })
+    }
+    return ctg;
+}
+
+function selectCtg() {
+    //Удалить все активные классы 
+    rmActive();
+    //Добавить активный класс
+    this.classList.toggle("type-restaurant_active");
+    //Показать все рестораны
+    fadeIn();
+
+    var ctg = restCtg();
+    var result = [];
+    var _id = this.id.toLowerCase();
+
+    //Проверка на категорию
+    if (this.id != "Все") {
+        ctg.forEach(function(item) {
+            if (!(item.category.indexOf(_id) != -1)) {
+                // Скрыть ресторан если не подходит
+                fadeOut(item.numRest);
+            }
+        })
+    }
+    return false;
+}
+
 //Функции анимации
 function fadeOut(num) {
-    let el = document.querySelector("[data-id-rest='" + num + "']");
+    var el = document.querySelector("[data-id-rest='" + num + "']");
     // el.style.opacity = 0.3;
     el.style.display = "none";
     el.style.pointerEvents = "none";
@@ -82,9 +83,9 @@ function fadeOut(num) {
 }
 
 function fadeIn(el, display) {
-    let hiddenItems = document.querySelectorAll("[data-rest-display='hide'");
+    var hiddenItems = document.querySelectorAll("[data-rest-display='hide']");
     if (hiddenItems.length != 0) {
-        for (let i = 0; i < hiddenItems.length; i++) {
+        for (var i = 0; i < hiddenItems.length; i++) {
             hiddenItems[i].style.opacity = 1;
             hiddenItems[i].style.display = "block";
             hiddenItems[i].style.pointerEvents = "all";
