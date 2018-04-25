eeedf96bbc7e81bcfff396d8ca69d355ef825250
Icon003 <Icon003@yandex.by>
2018-04-25 12:03:58 +0300

    add total price fire function

--- static/js/cart-generate-block.js
+++ static/js/cart-generate-block.js
@@ -11,9 +11,9 @@ function ShowCart() {
 		AjaxCardButton.classList.add("button-cart-post-disabled");
 	} else {
 		for (var i = 0; i < Bly.length; i++) {
-			if(Bly[i].PortionsStatus == "true"){
-			   div += '<div class="container-eat-cart"><div class="container-image-eat-cart"><img src="' + Bly[i].Image + '"><div class="container-name-eat-cart"><span>' + Bly[i].Name + '</span></div></div><div class="container-button-eat-cart"><div class="container-price-eat-cart"><span>0.5 - ' + Bly[i].Price4 + ' / 1 - '+ Bly[i].Price8 +' руб.</span></div><div class="card-dish-action"><div data-id-dish="' + Bly[i].id + '" data-portions-status-dish="true" class="inpMinus">-</div><input data-portions-status-dish="true" class="dishCount" type="number" step="0.5" min="0.5" max="50" name="dishCount" value="' + Bly[i].CountDish + '" readonly><div data-id-dish="' + Bly[i].id + '" data-portions-status-dish="true" class="inpPlus">+</div><div data-id-dish="' + Bly[i].id + '" class="container-button-cancel-eat-cart"><span class="fa fa-times"></span></div></div></div></div>';
-			}else{
+			if (Bly[i].PortionsStatus == "true") {
+				div += '<div class="container-eat-cart"><div class="container-image-eat-cart"><img src="' + Bly[i].Image + '"><div class="container-name-eat-cart"><span>' + Bly[i].Name + '</span></div></div><div class="container-button-eat-cart"><div class="container-price-eat-cart"><span>0.5 - ' + Bly[i].Price4 + ' / 1 - ' + Bly[i].Price8 + ' руб.</span></div><div class="card-dish-action"><div data-id-dish="' + Bly[i].id + '" data-portions-status-dish="true" class="inpMinus">-</div><input data-portions-status-dish="true" class="dishCount" type="number" step="0.5" min="0.5" max="50" name="dishCount" value="' + Bly[i].CountDish + '" readonly><div data-id-dish="' + Bly[i].id + '" data-portions-status-dish="true" class="inpPlus">+</div><div data-id-dish="' + Bly[i].id + '" class="container-button-cancel-eat-cart"><span class="fa fa-times"></span></div></div></div></div>';
+			} else {
 				div += '<div class="container-eat-cart"><div class="container-image-eat-cart"><img src="' + Bly[i].Image + '"><div class="container-name-eat-cart"><span>' + Bly[i].Name + '</span></div></div><div class="container-button-eat-cart"><div class="container-price-eat-cart"><span>' + Bly[i].Price + ' руб.</span></div><div class="card-dish-action"><div data-id-dish="' + Bly[i].id + '" data-portions-status-dish="false" class="inpMinus">-</div><input data-portions-status-dish="false" class="dishCount" type="number" step="1" min="1" max="50" name="dishCount" value="' + Bly[i].CountDish + '" readonly><div data-id-dish="' + Bly[i].id + '" data-portions-status-dish="false" class="inpPlus">+</div><div data-id-dish="' + Bly[i].id + '" class="container-button-cancel-eat-cart"><span class="fa fa-times"></span></div></div></div></div>';
 			}
 		}
@@ -37,9 +37,15 @@ function TotalCart() {
 		for (var i = 0; i < Bly.length; i++) {
 			Price += parseFloat(Bly[i].TotalPriceDish);
 		}
-		if(Price < 20){
-			if(Bly[0].NameRest != "Пиццерия 'Корица'"){
+		if (Bly[0].NameRest === "Огонь") {
+			if (Price < 10) {
 				Price += 2;
+			}
+		} else {
+			if (Price < 20) {
+				if (Bly[0].NameRest != "Пиццерия 'Корица'") {
+					Price += 2;
+				}
 			}
 		}
 		StrPrice = "Общая стоимость: " + Price.toFixed(2) + " руб."
