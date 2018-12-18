20b0404a208624e091ea8a9d4fe8534d5e2da880
Alexander <fel1kssx@gmail.com>
2018-12-18 17:49:28 +0300

    Скрыт слайдер, на Safari display none не помог

--- static/js/index.js
+++ static/js/index.js
@@ -1,21 +1,21 @@
 // Initialize Swiper
 document.addEventListener("DOMContentLoaded", function(event) {
-    var swiper = new Swiper('.swiper-container', {
-        slidesPerView: 1,
-        autoHeight: true, //enable auto height
-        spaceBetween: 0,
-        // setWrapperSize: true,
-        loop: true,
-        disableOnInteraction: false,
-        autoplay: {
-            delay: 4200,
-        },
+    // var swiper = new Swiper('.swiper-container', {
+    //     slidesPerView: 1,
+    //     autoHeight: true, //enable auto height
+    //     spaceBetween: 0,
+    //     // setWrapperSize: true,
+    //     loop: true,
+    //     disableOnInteraction: false,
+    //     autoplay: {
+    //         delay: 4200,
+    //     },
 
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-    });
+    //     pagination: {
+    //         el: '.swiper-pagination',
+    //         clickable: true,
+    //     },
+    // });
 
     addEventCtgRest();
 });
